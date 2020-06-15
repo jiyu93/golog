5a631a719ac288e23f4fc5c9ed13f6d0200e3453
jiyu.lin <jiyu.forest@gmail.com>
2020-06-15 10:44:19 +0800

    fix: 修复calldepth错误问题

--- logger.go
+++ logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 )
 
 // Log levels
@@ -49,48 +50,51 @@ func (l *Logger) SetFlags(flag int) {
 // Trace 输出Trace级别的日志
 func (l *Logger) Trace(v ...interface{}) {
 	if l.lv <= LevelTrace {
-		l.lg.Printf("[T] %s", fmt.Sprintln(v...))
+		l.lg.Output(3, fmt.Sprintf("[T] %s", fmt.Sprintln(v...)))
 	}
 }
 
 // Debug 输出Debug级别的日志
 func (l *Logger) Debug(v ...interface{}) {
 	if l.lv <= LevelDebug {
-		l.lg.Printf("[D] %s", fmt.Sprintln(v...))
+		l.lg.Output(3, fmt.Sprintf("[D] %s", fmt.Sprintln(v...)))
 	}
 }
 
 // Info 输出Info级别的日志
 func (l *Logger) Info(v ...interface{}) {
 	if l.lv <= LevelInfo {
-		l.lg.Printf("[I] %s", fmt.Sprintln(v...))
+		l.lg.Output(3, fmt.Sprintf("[I] %s", fmt.Sprintln(v...)))
 	}
 }
 
 // Warn 输出Warn级别的日志
 func (l *Logger) Warn(v ...interface{}) {
 	if l.lv <= LevelWarn {
-		l.lg.Printf("[W] %s", fmt.Sprintln(v...))
+		l.lg.Output(3, fmt.Sprintf("[W] %s", fmt.Sprintln(v...)))
 	}
 }
 
 // Error 输出Error级别的日志
 func (l *Logger) Error(v ...interface{}) {
 	if l.lv <= LevelError {
-		l.lg.Printf("[E] %s", fmt.Sprintln(v...))
+		l.lg.Output(3, fmt.Sprintf("[E] %s", fmt.Sprintln(v...)))
 	}
 }
 
 // Panic 输出Panic级别的日志，然后调用panic()函数
 func (l *Logger) Panic(v ...interface{}) {
 	if l.lv <= LevelPanic {
-		l.lg.Panicf("[P] %s", fmt.Sprintln(v...))
+		s := fmt.Sprintf("[P] %s", fmt.Sprintln(v...))
+		l.lg.Output(3, s)
+		panic(s)
 	}
 }
 
 // Fatal 输出Fatal级别的日志，然后直接退出程序
 func (l *Logger) Fatal(v ...interface{}) {
 	if l.lv <= LevelFatal {
-		l.lg.Fatalf("[F] %s", fmt.Sprintln(v...))
+		l.lg.Output(3, fmt.Sprintf("[F] %s", fmt.Sprintln(v...)))
+		os.Exit(1)
 	}
 }
